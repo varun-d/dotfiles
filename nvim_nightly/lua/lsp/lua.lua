3b5276f03d16bb124180e34acedc489a70aa8c32
Varun D <varun.dhanwantri@gmail.com>
2025-09-25 10:43:36 -0400

    Update Lua lsp and add stylua

--- nvim_nightly/lua/lsp/lua.lua
+++ nvim_nightly/lua/lsp/lua.lua
@@ -13,6 +13,13 @@ return {
         Lua = {
             runtime = {
                 version = 'LuaJIT',
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    '${3rd}/luv/library',
+                    unpack(vim.api.nvim_get_runtime_file('', true)),
+                }
             }
         }
     }
