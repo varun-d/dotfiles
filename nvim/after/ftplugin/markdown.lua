0eed854ca98ec4764f6235916b76d1cede14a902
Varun D <varun.dhanwantri@gmail.com>
2025-10-10 13:01:42 -0400

    Added simple highlighting for checkboxes in markdown, [ ] in yellow and [x] in green

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,1 +1,14 @@
+-- In your main init.lua or a plugin file
 vim.wo.wrap = true
+vim.api.nvim_create_autocmd("Syntax", {
+	pattern = "markdown",
+	callback = function()
+		vim.cmd([[
+        syntax match TodoDone /- \[x\]/
+        syntax match TodoEmpty /- \[ \]/
+
+        highlight link TodoDone DiagnosticOk
+        highlight link TodoEmpty DiagnosticWarn
+      ]])
+	end,
+})
