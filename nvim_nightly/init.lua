c315a7a1ce967cef619915f0d6bc6e5a253d68d5
Varun D <varun.dhanwantri@gmail.com>
2026-04-04 10:21:36 -0400

    Update .12 init

--- nvim_nightly/init.lua
+++ nvim_nightly/init.lua
@@ -1,6 +1,7 @@
 -- Set Leader Key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
 -- Import custom global options
 require("options")
 
@@ -26,64 +27,62 @@ require("mini.surround").setup()
 require("mini.pick").setup({})
 require("mini.extra").setup({})
 
-
 -- Which-key setup
 require("which-key").setup({
-    spec = {
-        { "<leader>s", group = "[S]earch" },
-        { "<leader>d", group = "[D]ebug" },
-        { "<leader>t", group = "[T]oggle" },
-        { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
-    },
-
+	spec = {
+		{ "<leader>s", group = "[S]earch" },
+		{ "<leader>d", group = "[D]ebug" },
+		{ "<leader>t", group = "[T]oggle" },
+		{ "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+	},
 })
 -- TODO: Treesitter setup
 local treesit = require("nvim-treesitter")
 treesit.install({
-    "bash",
-    "c",
-    "dockerfile",
-    "fish",
-    "git_config",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "go",
-    "gomod",
-    "gosum",
-    "html",
-    "javascript",
-    "json",
-    "lua",
-    "make",
-    "markdown",
-    "python",
-    "rust",
-    "sql",
-    "toml",
-    "tsx",
-    "typescript",
-    "yaml",
-    "zig",
+	"bash",
+	"c",
+	"dockerfile",
+	"fish",
+	"git_config",
+	"git_rebase",
+	"gitattributes",
+	"gitcommit",
+	"gitignore",
+	"go",
+	"gomod",
+	"gosum",
+	"html",
+	"javascript",
+	"json",
+	"lua",
+	"make",
+	"markdown",
+	"python",
+	"rust",
+	"sql",
+	"toml",
+	"tsx",
+	"typescript",
+	"yaml",
+	"zig",
 })
 
 -- Fidget LSP progress
-require('fidget').setup({})
+require("fidget").setup({})
 
 -- Oil file explorer
-require('oil').setup()
+require("oil").setup()
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Completions
-require('blink.cmp').setup({
-    fuzzy = { implementation = "prefer_rust_with_warning" },
-    completion = {
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 200,
-        }
-    },
+require("blink.cmp").setup({
+	fuzzy = { implementation = "prefer_rust_with_warning" },
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 200,
+		},
+	},
 })
 -- Color Scheme, switch it here
 vim.cmd.colorscheme("gruvbox-material")
