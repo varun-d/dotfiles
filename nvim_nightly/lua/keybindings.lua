05e7aca3c1702f91c243616f5777511ac488e1da
Varun D <varun.dhanwantri@gmail.com>
2026-04-04 10:21:11 -0400

    Update .12 keybindings

--- nvim_nightly/lua/keybindings.lua
+++ nvim_nightly/lua/keybindings.lua
@@ -1,31 +1,37 @@
--- My Keybindings
+-- Set shortname
 local setkmap = vim.keymap.set
-local s = {silent = true}
+local s = { silent = true }
 
--- Set Leader Key
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- Update Vim Pack Plugins
+setkmap("n", "<leader>u", "<cmd>[U]pdate Plugins<CR>")
 
 -- Temporary for neovim configuration builds
-setkmap("n", "<leader>b", ':update<CR> :source<CR>', { desc = "[B]uild config for local" })
+setkmap("n", "<leader>b", ":update<CR> :source<CR>", { desc = "[B]uild config for local" })
 
 -- HARD MODE ON
-setkmap("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-setkmap("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-setkmap("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-setkmap("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+setkmap("n", "<left>", '<cmd>echo "Use h to move left!"<CR>')
+setkmap("n", "<right>", '<cmd>echo "Use l to move right!"<CR>')
+setkmap("n", "<up>", '<cmd>echo "Use k to move up!"<CR>')
+setkmap("n", "<down>", '<cmd>echo "Use j to move down!"<CR>')
 
 -- Clear highlight on pressing Escape after a search
 setkmap("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Diagnostic Keymaps: TODO: Check if this is working fine later.
-setkmap("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Diagnostic [Q]uickfix list" })
+-- [D]iagnostic/[D]ebug Keymaps:
+setkmap("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "[D]iagnostic [Q]uickfix list" })
+
+-- Set wrap for the quickfix window
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	callback = function()
+		vim.opt_local.wrap = true
+	end,
+	group = vim.api.nvim_create_augroup("QuickfixWrap", { clear = true }),
+})
 
 -- QOL keymaps like <leader>w or x to write, quit etc.
 setkmap("n", "<leader>w", "<CMD>w<CR>", { desc = "[W]rite to file." })
 setkmap("n", "<leader>q", "<CMD>bd<CR>", { desc = "[Q]uit buffer." })
-
-setkmap("n", "<leader>u", '<cmd>lua vim.pack.update()<CR>')
 
 -- Set jk kj as ESC keys.
 setkmap("i", "jk", "<Esc>")
@@ -41,12 +47,11 @@ setkmap("n", "x", '"_x')
 setkmap("n", "<C-d>", "<C-d>zz", { desc = "Move down in buffer and center with zz" })
 setkmap("n", "<C-u>", "<C-u>zz", { desc = "Move up in buffer and center with zz" })
 
--- TODO: Add Windows and Tab split shortcuts as your workflow grows! Don't do it too early.
+-- Copy pasting helpers, for "+ buffer
+setkmap("n", "<leader>c", '"+yy', { desc = "[C]opy to clipboard" })
+setkmap({ "n", "x" }, "<leader>c", '"+yy<ESC>', { desc = "[C]opy to clipboard" })
+setkmap("n", "<leader>v", '"+p', { desc = "[V]Paste from clipboard" })
+
 -- Oil Keymaps
 setkmap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
--- LSP Keymaps
-setkmap("n", "<leader>lf", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
-
--- Debugger Keymaps
--- setkmap("n", "<leader>db", "<CMD>DapToggleBreakpoint<CR>", { desc = "Toggle [B]reakpoint" })
+-- map("n", "-", "<CMD>lua MiniFiles.open()<CR>", { desc = "Open parent directory" })
